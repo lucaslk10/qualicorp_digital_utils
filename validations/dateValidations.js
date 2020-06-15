5cb1e966669898bcd6770a7a4385b5f2dad841ae
Guilherme S. Andrade <guilherme_s_andrade@hotmail.com>
2020-06-14 23:07:48 -0300

    1.3.2 isValidEUAFormat

--- validations/dateValidations.js
+++ validations/dateValidations.js
@@ -1,6 +1,9 @@
 module.exports = {
-  isValidFormat(data) {
+  isValidBRFormat(data) {
     return new RegExp(/^\d{2}\/\d{2}\/\d{4}$/).test(data);
+  },
+  isValidEUAFormat(data) {
+    return new RegExp(/([12]\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01]))/).test(data);
   },
   isValidRange(dataInicio, dataFim) {
     return parseInt(dataFim) > parseInt(dataInicio);
