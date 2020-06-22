12c31b68945d14057a12bef5e01cc8057640743f
Guilherme S. Andrade <guilherme_s_andrade@hotmail.com>
2020-06-22 18:11:50 -0300

    1.3.9

--- conversions/dateConversions.js
+++ conversions/dateConversions.js
@@ -6,14 +6,14 @@ module.exports = {
     return moment(date).format("DD/MM/YYYY");
   },
   formatEUA(value) {
-    if (!((typeof value === "string") || (typeof value.getMonth === "function"))) {
+    if (!((typeof value === "string") || (typeof value === "number") || (typeof value.getMonth === "function"))) {
       throw new AppError("Formato de Data inválido.")
     }
 
     return moment(value).format("YYYY-MM-DD");
   },
   formatEUAdateTime(value) {
-    if (!((typeof value === "string") || (typeof value.getMonth === "function"))) {
+    if (!((typeof value === "string") || (typeof value === "number") || (typeof value.getMonth === "function"))) {
       throw new AppError("Formato de Data inválido.")
     }
 
