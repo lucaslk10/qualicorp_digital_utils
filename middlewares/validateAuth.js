c44e231d320261e866e179e5c316fb1f02d166ca
Guilherme S. Andrade <guilherme_s_andrade@hotmail.com>
2020-07-22 14:12:59 -0300

    1.6.2 validateAuth livre

--- middlewares/validateAuth.js
+++ middlewares/validateAuth.js
@@ -4,20 +4,15 @@ const axios = require("axios");
 
 exports.validateAuth = async function (req, res, next) {
   let { auth } = req.headers;
-  let { noauth = false } = req.body;
-
-  if (noauth) {
-    next();
-    return;
-  }
 
   if (!process.env.URL_TOKEN_VALIDAR) {
     throw new AppError("Variável de ambiente 'URL_TOKEN_VALIDAR' não foi encontrada.");
   }
 
-  if (!auth) {
-    throw new AuthError("'auth' não foi preenchido.");
-  }
+  // Essa linha é válida, ela foi comentada provisoriamente
+  //if (!auth) {
+  //  throw new AuthError("'auth' não foi preenchido.");
+  //}
 
   let response = undefined;
   try {
@@ -27,7 +22,10 @@ exports.validateAuth = async function (req, res, next) {
       error.response.data &&
       ((error.response.data.message.toLowerCase() == "authorization has been denied for this request.") ||
         (error.response.data.message.toLowerCase() == "recusado"))) {
-      throw new AuthError("'auth' inválido ou vencido.");
+      // Essa linha é válida, ela foi comentada provisoriamente
+      // throw new AuthError("'auth' inválido ou vencido.");
+      next(); // Essa linha deverá ser retirada quando a linha de cima for descomentada
+      return; // Essa linha deverá ser retirada quando a linha de cima for descomentada
     }
     throw error;
   }
