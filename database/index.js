a3399b29849c9e8bc58766f5e4ba4bb98187d44e
Guilherme S. Andrade <guilherme_s_andrade@hotmail.com>
2020-07-22 11:34:32 -0300

    1.6.1 - mssql

--- database/index.js
+++ database/index.js
@@ -1,5 +1,7 @@
 const neo4j = require("./neo4j");
+const mssql = require("./mssql");
 
 module.exports = {
-  neo4j: neo4j
+  neo4j: neo4j,
+  mssql: mssql
 }
---
+++ database/mssql.js
@@ -0,0 +1,69 @@
+const mssql = require("mssql");
+
+class Qmssql {
+  static create({ user, password, server, port, database, appName }) {
+    return new Qmssql({ user, password, server, port, database, appName });
+  }
+
+  constructor({ user, password, server, port, database, appName }) {
+    this.db = undefined;
+    this.config = {
+      user: user,
+      password: password,
+      server: server,
+      port: parseInt(port) || 1446,
+      database: database,
+      parseJSON: true,
+      options: {
+        encrypt: true,
+        appName: appName
+      }
+    };
+  }
+
+  async connect() {
+    try {
+      this.db = await new mssql.ConnectionPool(this.config).connect();
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  isConnected() {
+    return !!this.db._connected;
+  }
+
+  async query(sql, params = []) {
+    try {
+      await this.connect();
+      const request = this.db.request();
+      for (const p of params) {
+        request.input(p.name, p.value);
+      }
+      const result = await request.query(sql);
+      return result.recordset;
+    } catch (error) {
+      throw error;
+    } finally {
+      if (this.db) await this.db.close();
+    }
+  }
+
+  async execute(statement, params = []) {
+    try {
+      await this.connect();
+      const request = this.db.request();
+      for (const p of params) {
+        request.input(p.name, p.value);
+      }
+      const result = await request.query(statement);
+      return result.rowsAffected[0] > 0;
+    } catch (error) {
+      throw error;
+    } finally {
+      if (this.db) await this.db.close();
+    }
+  }
+}
+
+module.exports = Qmssql;
