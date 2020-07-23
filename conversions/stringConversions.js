07cad3788ba6219561c436c7d573de276435557e
Guilherme S. Andrade <guilherme_s_andrade@hotmail.com>
2020-07-23 10:51:55 -0300

    1.7.0 hasFileExtension

--- conversions/stringConversions.js
+++ conversions/stringConversions.js
@@ -1,5 +1,11 @@
 module.exports = {
   getOnlyNumbers(str) {
     return str.replace(/[^\d]+/g, '')
+  },
+  hasFileExtension(fileName) {
+    const re = /(?:\.([^.]+))?$/;
+    const ext = re.exec(fileName)[1];
+
+    return (!!ext);
   }
 }
